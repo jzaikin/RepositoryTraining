654068de0e2d7adfee885eb02e4361a6ba81aa8f
Jesse <jzaikin@msudenver.edu>
2018-09-13 13:09:03 -0600

    Initial commit

---
+++ Stacks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(countStack(1, 500000));
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine("oops");
+            }
+            Console.ReadKey();
+        }
+
+        public static int countStack(int i, int amount)
+        {
+            
+                if (i >= amount)
+                    return i;
+               return countStack(++i, amount);
+            
+        }
+    }
+}
